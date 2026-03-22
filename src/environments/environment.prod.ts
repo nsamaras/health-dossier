6f180665b82f762295da03e50fc9b68ba1905195
SamarasN <samarasn@unisystems.gr>
2026-03-22 16:37:24 +0200

    commit environment files

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  production: true,
+  firebase: {
+    
+    apiKey: "AIzaSyCHYpYeaoN9X4R5lnrxlNqxUXo5mhRUmJ8",
+    authDomain: "health-dossier.firebaseapp.com",
+    projectId: "health-dossier",
+    storageBucket: "health-dossier.appspot.com",
+    messagingSenderId: "315638128873",
+    appId: "1:315638128873:web:7a86324f7c2854cccd5078"
+
+  },
+  api: {
+
+  }
+};
